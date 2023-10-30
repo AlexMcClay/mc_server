aeb7eed8eced58179f34188b674757cf922eadb8
Alex McClay <alex.mcclay2000@gmail.com>
2023-10-31 06:26:03 +1030

    asd

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx6G -jar fabric-server-mc.1.20.2-loader.0.14.23-launcher.0.11.2.jar nogui
+java -Xmx6G -Xms4024m -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=4M -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -jar fabric-server-mc.1.20.2-loader.0.14.23-launcher.0.11.2.jar nogui
