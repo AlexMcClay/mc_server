658e0dd304136499bc6fe372017a978c7c34284f
Alex McClay <alex.mcclay2000@gmail.com>
2023-11-02 03:50:12 +1030

    renamed server

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -Xmx8G -jar fabric-server-mc.1.20.2-loader.0.14.23-launcher.0.11.2.jar nogui
+java -Xmx8G -jar server.jar nogui
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx6G -Xms2G -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=4M -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -jar fabric-server-mc.1.20.2-loader.0.14.23-launcher.0.11.2.jar nogui
+java -Xmx6G -Xms2G -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=4M -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -jar server.jar nogui
